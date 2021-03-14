e651099668ea3aa6e0e69c716b40e2212996a872
Robert Colville <rkcolville@gmail.com>
2021-03-14 10:35:10 -0500

    initial commit

---
+++ Syntax and Syntax Sugar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Syntax_and_Syntax_Sugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + " greater than or equal to nine";
+            }
+            Console.WriteLine(response);
+                
+ 
+
+        }   
+    }
+}
+
+           
